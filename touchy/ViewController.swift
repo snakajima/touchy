b810b7a9fc734c3cf51a70c9abad02aabbfd3d76
Satoshi Nakajima <satoshi@uievolution.com>
2018-08-08 09:56:52 -0700

    box draw and clear

--- touchy/ViewController.swift
+++ touchy/ViewController.swift
@@ -19,7 +19,33 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  @IBAction func clear(_ sender:UIBarButtonItem) {
+    for subview in view.subviews {
+      subview.removeFromSuperview()
+    }
+  }
 
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    print("touchBegan")
+  }
 
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    let loc = touch.location(in: view)
+    print("touchMoved", loc.x, loc.y)
+    let rc = CGRect(origin: loc, size: .zero).insetBy(dx: -10, dy: -10)
+    let subview = UIView(frame: rc)
+    subview.backgroundColor = UIColor.red
+    view.addSubview(subview)
+  }
+  
+  override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    print("touchCancelled")
+  }
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    print("touchEnded")
+  }
+  
 }
 
